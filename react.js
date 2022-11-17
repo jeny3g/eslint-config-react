7bdcf104a19dc762de602d2a5362b8d18d911c8e
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-11-17 10:38:22 -0300

    feat: :sparkles: feat: config parser

--- react.js
+++ react.js
@@ -14,6 +14,7 @@ module.exports = {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
